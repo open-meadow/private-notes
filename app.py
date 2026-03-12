6739963a52b224719705b97d11d13b7dc2a57548
open-meadow <rohan.varughese200@gmail.com>
2026-03-12 13:54:42 -0400

    Create mock data json file

--- app.py
+++ app.py
@@ -1,12 +1,14 @@
 from fastapi import FastAPI
+import json
 
 app = FastAPI()
 
 @app.get("/")
 def main():
-    print("Hello from private-notes!")
-
-    return {"Hello": "World"}
+    with open("data/MOCK_DATA.json", "r") as mock_data:
+        print(mock_data)
+    
+    return {"hello": "world"}
 
 
 if __name__ == "__main__":
