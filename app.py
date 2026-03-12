2018475e2591025e369392fe1798546fe66531d4
open-meadow <rohan.varughese200@gmail.com>
2026-03-12 13:50:11 -0400

    rename main.py to app.py

--- app.py
+++ app.py
@@ -1,1 +1,13 @@
-print("hello world back-end")
+from fastapi import FastAPI
+
+app = FastAPI()
+
+@app.get("/")
+def main():
+    print("Hello from private-notes!")
+
+    return {"Hello": "World"}
+
+
+if __name__ == "__main__":
+    main()
