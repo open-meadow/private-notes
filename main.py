d8d0bb9deaae33dd9c28dcfef2c57602be46da2b
open-meadow <rohan.varughese200@gmail.com>
2026-03-12 13:47:09 -0400

    Initialize Hello-World root endpoint

--- main.py
+++ main.py
@@ -1,5 +1,12 @@
+from fastapi import FastAPI
+
+app = FastAPI()
+
+@app.get("/")
 def main():
     print("Hello from private-notes!")
+
+    return {"Hello": "World"}
 
 
 if __name__ == "__main__":
